b3b8d81661145c67b2032df92728cd7859ae1c00
Martin Dengis <martin.dengis@gmail.com>
2025-03-27 16:10:24 +0000

    Fix perf buffer output

--- page_faults/src/page_fault.bpf.c
+++ page_faults/src/page_fault.bpf.c
@@ -5,9 +5,6 @@
 #include <bpf/bpf_core_read.h>
 #include "page_fault.h"
 
-#ifndef TASK_COMM_LEN
-#define TASK_COMM_LEN 16
-#endif
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
@@ -39,6 +36,12 @@ const volatile int log_step = 50;
 // Data structures
 // ****************************************
 
+// Tuple to act as the values of the HASH map used to track fault counts for each pid
+struct fault_tracking {
+    unsigned int pg_fault_count;     // Curr fault count
+    char comm[TASK_COMM_LEN];      // Process name
+};
+
 // The hash map to track the fault_count of a pid
 // map[pid]=(pg_fault_count, process name)
 struct {
@@ -60,19 +63,26 @@ struct {
 // Custom helpers
 // ****************************************
 
-static int buffer_out(void *ctx, u32 pid, char *comm, u64 page_fault_count)
+int buffer_out(void *ctx, int pid)
 {
-    // create the out object following `page_fault_event_out` structure
-    struct page_fault_event_out out_this = {
-        .timestamp = bpf_ktime_get_ns(),
-        .pid = pid,
-        .page_fault_count = page_fault_count
-    };
-    bpf_probe_read_str(out_this.comm, sizeof(out_this.comm), comm);
+    // retrieve data associated to given pid
+    struct fault_tracking *ft = bpf_map_lookup_elem(&mapping, &pid);
+    if (ft)
+    {
+        // create the out object following `page_fault_event_out` structure
+        struct page_fault_event_out out_this = {0};
+        out_this.pid = pid;
+        out_this.page_fault_count = ft->pg_fault_count;
 
-    // output this
-    bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &out_this, sizeof(out_this));
-    return 0;
+        bpf_probe_read_str(&out_this.comm, sizeof(out_this.comm), ft->comm);
+
+        // output this
+        bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &out_this, sizeof(out_this));
+        return 0;
+    }
+
+    // failure
+    return -1;
 }
 
 
@@ -86,21 +96,21 @@ int BPF_KPROBE(handle_mm_fault, struct vm_area_struct *vma, unsigned long addres
                unsigned int flags, struct pt_regs *regs)
 {
     // get current pid (shifted by 32 bits to exlude tgid)
-    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    pid_t pid;
+    pid = bpf_get_current_pid_tgid() >> 32;
 
     // find in `mapping` if pid entry already exists, else add it
     struct fault_tracking *ft = bpf_map_lookup_elem(&mapping, &pid);
     if (ft == NULL) {
         // entry does not exist yet in the mapping, so:
         // 1. init new `fault_tracking` tuple for the pid
-        struct fault_tracking new_ft = {
-            .pg_fault_count = 1,
-        };
+        struct fault_tracking new_ft = {0};
+        new_ft.pg_fault_count = 1;
 
         // 2. get process name and add it to the `fault_tracking` tuple
         char comm[TASK_COMM_LEN];
         bpf_get_current_comm(&comm, sizeof(comm));
-        bpf_probe_read_str(new_ft.comm, sizeof(new_ft.comm), comm);
+        bpf_probe_read_str(&new_ft.comm, sizeof(new_ft.comm), comm);
 
         // 3. update the mapping with the new entry
         bpf_map_update_elem(&mapping, &pid, &new_ft, BPF_ANY);
@@ -108,7 +118,7 @@ int BPF_KPROBE(handle_mm_fault, struct vm_area_struct *vma, unsigned long addres
         // then we check if log_step == 1 because if it is, we already need to log in the console
         if (log_step == 1)
         {
-            buffer_out(ctx, pid, new_ft.comm, 1);
+            buffer_out(ctx, pid);
         }
     } else {
         // entry already exists -> increment fault count
@@ -117,10 +127,9 @@ int BPF_KPROBE(handle_mm_fault, struct vm_area_struct *vma, unsigned long addres
         // check if new pg_fault_count is a multiple of log_step
         if (ft->pg_fault_count % log_step == 0)
         {
-            buffer_out(ctx, pid, ft->comm, ft->pg_fault_count);
+            buffer_out(ctx, pid);
         }
     }
-
+    
     return 0;
 }
-
--- page_faults/src/page_fault.h
+++ page_faults/src/page_fault.h
@@ -1,22 +1,15 @@
 #ifndef PAGE_FAULT_H
 #define PAGE_FAULT_H
 
-#ifndef TASK_COMM_LEN
-#define TASK_COMM_LEN 16
-#endif
+#define TASK_COMM_LEN 20
 
-// Tuple to act as the values of the HASH map used to track fault counts for each pid
-struct fault_tracking {
-    __u64 pg_fault_count;     // Curr fault count
-    char comm[TASK_COMM_LEN];      // Process name
-};
 
 // Struct sent to the perf buffer for printing
 struct page_fault_event_out {
-    __u64 timestamp;
-    __u32 pid;
+    pid_t pid;
     char comm[TASK_COMM_LEN];
-    __u32 page_fault_count;
+    int page_fault_count;
 };
+
 
 #endif // PAGE_FAULT_H
